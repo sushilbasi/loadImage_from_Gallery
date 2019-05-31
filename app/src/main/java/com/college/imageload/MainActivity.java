8bef0a29662b2aa20592c745365356659548bc37
Sushil Basi <basisushil@gmail.com>
2019-05-31 23:48:34 +0545

    loadImage_from_Gallary

---
+++ app/src/main/java/com/college/imageload/MainActivity.java
@@ -0,0 +1,83 @@
+package com.college.imageload;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import android.provider.MediaStore.MediaColumns;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends Activity {
+
+ 
+    public static final int PICK_IMAGE = 20;
+    ImageView imageView;
+    Button button;
+    private String selectedImagePath = " ";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = (ImageView) findViewById(R.id.imageView1);
+
+        Button butGallery = (Button) findViewById(R.id.btn_browse);
+        butGallery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                String pictureDirectoryPath = pictureDirectory.getPath();
+                Uri data = Uri.parse(pictureDirectoryPath);
+                intent.setDataAndType(data,"image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(
+                        Intent.createChooser(intent, ""),
+                        PICK_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK)
+        {
+            if(requestCode == PICK_IMAGE)
+            {
+                Uri imageUri = data.getData();
+                InputStream inputStream;
+                
+                try{
+                    inputStream = getContentResolver().openInputStream(imageUri);
+                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(image);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Unable to open image", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
